4017c8934a74ca301c134d162998bb406b15cec3
alvi2496 <alvi.utsab@gmail.com>
2018-01-17 14:01:33 +0600

    complete skills feature

---
+++ app/views/api/v1/skills/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.skills @skills do |skill|
+  json.id skill.id
+  json.name skill.name
+  json.level skill.level
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117063319) do
+ActiveRecord::Schema.define(version: 20180117070122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20180117063319) do
     t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
